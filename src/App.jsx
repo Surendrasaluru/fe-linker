02d69b79c3630928c8041651bcf19db0bbe63a58
Surendra <surendrasaluru@gmail.com>
2026-03-12 20:07:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;

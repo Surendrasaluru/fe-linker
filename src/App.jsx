ac2f03f49420d2c7213f5bc0c6cde294cf7c96fc
Surendra <surendrasaluru@gmail.com>
2026-03-12 20:41:15 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-    </div>
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Linker❤️</a>
+        <a className="btn btn-ghost text-xl">StackMate ❤️</a>
       </div>
       <div className="flex gap-2">
         <div className="dropdown dropdown-end mr-5">
